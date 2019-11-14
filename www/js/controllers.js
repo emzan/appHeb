bd2eb9281f82048b99349e79db498fcf98b37152
emzan <zanettiemilio@hotmail.com>
2019-11-14 16:32:03 +0800

    nonTiscordar

---
+++ www/js/controllers.js
@@ -0,0 +1,42 @@
+angular.module('app.controllers', [])
+  
+.controller('dizionarioCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('sintassiCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('lectioCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+      
+.controller('alephListaNomiCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+   
+.controller('tutorialCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+
+
+}])
+ 
---
+++ www/js/routes.js
@@ -0,0 +1,71 @@
+angular.module('app.routes', [])
+
+.config(function($stateProvider, $urlRouterProvider) {
+
+  // Ionic uses AngularUI Router which uses the concept of states
+  // Learn more here: https://github.com/angular-ui/ui-router
+  // Set up the various states which the app can be in.
+  // Each state's controller can be found in controllers.js
+  $stateProvider
+    
+
+      .state('tabsController.dizionario', {
+    url: '/page2',
+    views: {
+      'tab1': {
+        templateUrl: 'templates/dizionario.html',
+        controller: 'dizionarioCtrl'
+      }
+    }
+  })
+
+  .state('tabsController.sintassi', {
+    url: '/page3',
+    views: {
+      'tab2': {
+        templateUrl: 'templates/sintassi.html',
+        controller: 'sintassiCtrl'
+      }
+    }
+  })
+
+  .state('tabsController.lectio', {
+    url: '/page4',
+    views: {
+      'tab3': {
+        templateUrl: 'templates/lectio.html',
+        controller: 'lectioCtrl'
+      }
+    }
+  })
+
+  .state('tabsController', {
+    url: '/page1',
+    templateUrl: 'templates/tabsController.html',
+    abstract:true
+  })
+
+  .state('tabsController.alephListaNomi', {
+    url: '/page5',
+    views: {
+      'tab1': {
+        templateUrl: 'templates/alephListaNomi.html',
+        controller: 'alephListaNomiCtrl'
+      }
+    }
+  })
+
+  .state('tabsController.tutorial', {
+    url: '/page6',
+    views: {
+      'tab1': {
+        templateUrl: 'templates/tutorial.html',
+        controller: 'tutorialCtrl'
+      }
+    }
+  })
+
+$urlRouterProvider.otherwise('/page1/page2')
+
+
+});
